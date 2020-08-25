f8cf0f51de336876715c6eddc665115554d83a43
davidpropfin <letuananh.it1405@gmail.com>
2020-08-25 18:18:06 +0700

    create customer mapper

---
+++ src/main/java/com/scscyber/fur/mybatis/mapper/CustomerMapper.java
@@ -0,0 +1,9 @@
+package com.scscyber.fur.mybatis.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.scscyber.fur.repository.CustomerRepository;
+@Mapper
+public interface CustomerMapper extends CustomerRepository {
+
+}
